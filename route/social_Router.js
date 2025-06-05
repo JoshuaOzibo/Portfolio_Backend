4098d9bb39322e4a13ffab998d4272a3a9a15f89
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-06-05 10:44:00 +0100

    Add CRUD operations to social_Router.js: Implement routes for fetching, updating, and deleting social entries.

--- route/social_Router.js
+++ route/social_Router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createSocial } from "../controllers/social_controller.js";
+import { createSocial, getAllSocials, updateSocial, deleteSocial } from "../controllers/social_controller.js";
 
 const router = Router();
 
 router.post("/create", createSocial);
+router.get("/", getAllSocials);
+router.put("/:id", updateSocial);
+router.delete("/:id", deleteSocial);
 
 export default router;
