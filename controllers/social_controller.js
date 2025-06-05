620c56a1050ec334c233e03d12df598fee7811bd
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-06-05 10:42:54 +0100

    Add updateSocial and deleteSocial functions to social_controller.js for managing social entries. Enhance error handling and export updated functions.

--- controllers/social_controller.js
+++ controllers/social_controller.js
@@ -43,4 +43,40 @@ const getAllSocials = catchAsync(async (req, res) => {
     throw new ApiError(500, error.message);
   }
 });
-export { createSocial, getAllSocials };
+
+
+const updateSocial = catchAsync(async (req, res) => {
+  try {
+    const { name, link } = req.body;
+    const social = await Social.findByIdAndUpdate(req.params.id, { name, link }, { new: true });
+    if (!social) {
+      throw new ApiError(404, "Social not found");
+    }
+    res.status(200).json({
+      message: "Social updated successfully",
+      status: "success",
+      data: {
+        social,
+      },
+    });
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+const deleteSocial = catchAsync(async (req, res) => {
+  try {
+    const social = await Social.findByIdAndDelete(req.params.id);
+    if (!social) {
+      throw new ApiError(404, "Social not found");
+    }
+    res.status(200).json({
+      message: "Social deleted successfully",
+      status: "success",
+    });
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+export { createSocial, getAllSocials, updateSocial, deleteSocial };
